a116b3f8f1db55661ae71fb0345721a936b57c16
AnastasiiaZhyltsova <anastasiiazhyltsova@gmail.com>
2022-05-19 00:12:56 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+    controls: document.querySelector('div#controls'),
+    input: document.querySelector('input'),
+    buttonBoxesCreate: document.querySelector('[data-create]'),
+    buttonBoxesDestroy: document.querySelector('[data-destroy]'),
+    boxes: document.querySelector('div#boxes'),
+}  
+
+refs.buttonBoxesCreate.addEventListener('click', createBoxes)
+function createBoxes(amount) {
+    const elementDiv = document.createElement('div');
+    elementDiv.style.width = '30px';
+    elementDiv.style.height = '30px';
+    elementDiv.style.backgroundColor = getRandomHexColor();
+    refs.boxes.append(elementDiv);  
+}
+
+refs.buttonBoxesDestroy.addEventListener('click', destroyBoxes)
+
+function destroyBoxes() { 
+    boxes.innerHTML = '';
+
+}
+
+console.log(refs.createBoxes);
